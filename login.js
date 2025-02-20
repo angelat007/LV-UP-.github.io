a52b6db964d3299fbaf3a6a856517c91261eb374
PLAYER <angeltrinidad5758@gmail.com>
2025-02-20 09:50:34 -0400

    añado advertencia al iniciar sesion

--- login.js
+++ login.js
@@ -1,29 +1,32 @@
 const $submit = document.getElementById("submit"),
-      $password = document.getElementById("password"),
-      $username = document.getElementById("username"),
-      $visible = document.getElementById("visible");
+    $password = document.getElementById("password"),
+    $username = document.getElementById("username"),
+    $visible = document.getElementById("visible");
 
 //verificar si hay sesion iniciarda//
 if (localStorage.getItem('username')) {
-        loginDiv.style.display = 'none';
-        contentDiv.style.display = 'block';
-}
+    loginDiv.style.display = 'none';
+    contentDiv.style.display = 'block';
+};
 
 //sivibilidad de contraseña//
-document.addEventListener("change", (e)=>{
-    if(e.target === $visible){
-        if($visible.checked === false) $password.type = "password";
+document.addEventListener("change", (e) => {
+    if (e.target === $visible) {
+        if ($visible.checked === false) $password.type = "password";
         else $password.type = "text";
     }
 });
 
 //boton de iniciar seccion//
-document.addEventListener ("click", (e)=>{
-    if(e.target === $submit){
-        if ($password.value !== "" && $username.value !== ""){
+document.addEventListener("click", (e) => {
+    if (e.target === $submit) {
+        if ($password.value !== "" && $username.value !== "") {
             e.preventDefault();
             window.location.href = "index.html";
         }
     }
-})
+});
 
+function SeguroQueQuieresIinicar() {
+    alert("Seguro que quieres iniciar sesion??")
+}
